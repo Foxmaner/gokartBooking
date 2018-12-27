a05db8d09d309d57b898c5290251de742462c23d
Foxmaner <45701690+Foxmaner@users.noreply.github.com>
2018-12-27 12:59:48 +0100

    #0.0.1 - Working on loginPage

---
+++ html/loginPage.php
@@ -0,0 +1,8 @@
+<div id="wrapperLogin" class="wrapper">
+  <div id="wrapperLoginWindow" class="wrapper col-s-4 col-4">
+    <form method="post" action="">
+      <input id="inputPassword" type="text" placeholder="Lösenord" name="password">
+      <input id="submitLogin" type="submit">
+    </form>
+  <div>
+</div>
--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Glabo Bokningssida</title>
+    <link rel="stylesheet" type="text/css" href="style/myStyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
-
+    <div class="wrapper">
+      <?php
+        require 'html/loginPage.php';
+      ?>
+    </div>
   </body>
 </html>
