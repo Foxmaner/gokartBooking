3820dca73b77c219e98d009caf002891d5c04434
Foxmaner <45701690+Foxmaner@users.noreply.github.com>
2018-12-28 01:39:28 +0100

    0.2.1 - Done with menuPage

---
+++ html/menuPage.php
@@ -0,0 +1,6 @@
+<div id="wrapperMenu" class="wrapper">
+    <div class="menuPageLinkWrappers col-s-2 col-2" id="menuPageLinkWrapper1" ><a href="analysPage.php">Analys</a></div>
+    <div class="menuPageLinkWrappers col-s-2 col-2" id="menuPageLinkWrapper2"><a href="kassaPage.php">Kassa</a></div>
+    <div class="menuPageLinkWrappers col-s-2 col-2" id="menuPageLinkWrapper3"><a href="depoPage.php">Depo</a></div>
+    <div class="menuPageLinkWrappers col-s-2 col-2" id="menuPageLinkWrapper4"><a href="settingsPage.php">Inställningar</a></div>
+</div>
