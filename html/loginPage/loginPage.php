cf3c02a4bb8c29708e7a1e36150359d1e9254711
foxmaner <eskil.brann@gmail.com>
2019-02-07 12:25:11 +0100

    0.7.1.2[Login/MenuPage] Added -Nice style to menu page Bugfix -Body was only 50% wide with 100% in css. Fixed

--- html/loginPage/loginPage.php
+++ html/loginPage/loginPage.php
@@ -1,11 +1,11 @@
 <div id="loginBackground"></div>
 <div id="wrapperLogin" class="wrapper col-s-12 col-12">
-  <div id="wrapperTransparentLoginWindow" class="wrapper col-s-3 col-3">
+  <div id="wrapperTransparentLoginWindow" class="wrapper col-s-12 col-3">
     <div id="loginTitleIcon"></div>
     <h1 class="title col-s-12 col-12">Glabo Gokart Bokningssytem</h1>
 
     <form id="loginForm" class="col-s-12 col-12" method="post" action="">
-      <input id="inputPassword" class="col-s-5 col-5" type="password" placeholder="Lösenord" name="passwordData">
+      <input id="inputPassword" class="col-s-10 col-7" type="password" placeholder="Lösenord" name="passwordData">
       <br>
         <!-- <a href="lool.php" class="col-s-12 col-12">Glömt lösenord?</a>-->
       <br>
