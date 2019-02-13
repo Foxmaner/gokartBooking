dbef845823a20ffe8ef9d10529c048794ad20b62
foxmaner <eskil.brann@gmail.com>
2019-02-13 10:10:43 +0100

    0.7.4.6[MenuPage] Changes -Typo

--- html/menuPage/menuPage.php
+++ html/menuPage/menuPage.php
@@ -2,7 +2,7 @@
 <div id="wrapperMenu" class="wrapper col-s-8 col-4">
     <div class="menuPageLinkWrappers" id="menuPageLinkWrapper1" ><a class="menuPageLinkText" href="analysPage.php">Analys</a></div>
     <div class="menuPageLinkWrappers" id="menuPageLinkWrapper2"><a class="menuPageLinkText" href="html/cashPage/cashPage.php">Kassa</a></div>
-    <div class="menuPageLinkWrappers" id="menuPageLinkWrapper3"><a class="menuPageLinkText" href="html/depotPage/depotPage.php">Depo</a></div>
+    <div class="menuPageLinkWrappers" id="menuPageLinkWrapper3"><a class="menuPageLinkText" href="html/depotPage/depotPage.php">Depå</a></div>
     <div class="menuPageLinkWrappers" id="menuPageLinkWrapper4"><a class="menuPageLinkText" href="html/settingPage/settingPage.php">Inställningar</a></div>
     <div class="menuPageLinkWrappers" id="menuPageLinkWrapper5"><a class="menuPageLinkText" href="html/logoutPage/logoutPage.php">Logout</a></div>
 </div>
