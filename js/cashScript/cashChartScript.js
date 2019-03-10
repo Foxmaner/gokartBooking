10fc90e1bb451ce6b32ba33a50c399cffeb65ccd
Foxmaner <eskil.brann@gmail.com>
2019-03-10 23:12:33 +0100

    x-annotation bug fix#1

--- js/cashScript/cashChartScript.js
+++ js/cashScript/cashChartScript.js
@@ -1,8 +1,7 @@
-
 //This shit is slow!
 function getRaceData() {
 
-var raceNr = document.getElementById("outputEditLopp").innerHTML;
+  var raceNr = document.getElementById("outputEditLopp").innerHTML;
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -19,76 +18,75 @@ var raceNr = document.getElementById("outputEditLopp").innerHTML;
 
 
 var options = {
-    chart: {
-        height: 350,
-        type: 'bar',
-        stacked: true,
-        toolbar: {
-            show: true
-        },
+  chart: {
+    height: 350,
+    type: 'bar',
+    stacked: true,
+    toolbar: {
+      show: true
     },
-    responsive: [{
-        breakpoint: 480,
-        options: {
-            legend: {
-                position: 'bottom',
-                offsetX: -10,
-                offsetY: 0
-            }
-        }
-    }],
-    plotOptions: {
-        bar: {
-            horizontal: false,
-        },
-    },
-    series: [{
-        name: 'Stora',
-        data: []
-    },{
-        name: 'Små',
-        data: []
-    },{
-        name: 'Dubbla',
-        data: []
-    }],
-    yaxis: {
-      min: 0,
-      max: 12,
-      tickAmount: 6,
-      labels: {
-            show: false,
-        },
-    },
-    annotations: {
-    yaxis: [
-      {
-        y: 10,
-        borderColor: "red",
-        label: {
-          borderColor: "black",
-          style: {
-            color: "#fff",
-            background: "red"
-          },
-          text: "Maxgräns"
-        }
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      legend: {
+        position: 'bottom',
+        offsetX: -10,
+        offsetY: 0
       }
-    ]},
-    legend: {
-        show: false,
+    }
+  }],
+  plotOptions: {
+    bar: {
+      horizontal: false,
     },
-    fill: {
-        opacity: 1
+  },
+  series: [{
+    name: 'Stora',
+    data: []
+  }, {
+    name: 'Små',
+    data: []
+  }, {
+    name: 'Dubbla',
+    data: []
+  }],
+  yaxis: {
+    min: 0,
+    max: 12,
+    tickAmount: 6,
+    labels: {
+      show: false,
     },
+  },
+  annotations: {
+    yaxis: [{
+      y: 10,
+      borderColor: "red",
+      label: {
+        borderColor: "black",
+        style: {
+          color: "#fff",
+          background: "red"
+        },
+        text: "Maxgräns"
+      }
+    }]
+  },
+  legend: {
+    show: false,
+  },
+  fill: {
+    opacity: 1
+  },
 }
 
 var chart = new ApexCharts(
-    document.querySelector("#myChart"),
-    options
+  document.querySelector("#myChart"),
+  options
 );
 
-function loadChart(){
+function loadChart() {
   chart.render();
   updateChart();
 }
@@ -107,64 +105,69 @@ function createDatasets(obj) {
   dataPack3 = [];
   raceNr = [];
 
- for (var i = 0; i < obj.length; i++) {
-   dataPack1[i] = obj[i].largeKart;
-   dataPack2[i] = obj[i].smallKart;
-   dataPack3[i] = obj[i].doubleKart;
-   raceNr[i] = parseInt(obj[i].raceNr, 10);
- }
+  for (var i = 0; i < obj.length; i++) {
+    dataPack1[i] = obj[i].largeKart;
+    dataPack2[i] = obj[i].smallKart;
+    dataPack3[i] = obj[i].doubleKart;
+    raceNr[i] = parseInt(obj[i].raceNr, 10);
+  }
 
- if (obj.length<11) {
-   var startNr = +raceNr[obj.length-1]+1;
-   for (var i = obj.length+1; i <= 11; i++) {
-     dataPack1.push(0);
-     dataPack2.push(0);
-     dataPack3.push(0);
-     raceNr.push(startNr);
-     startNr++;
-   }
- }
+  if (obj.length < 11) {
+    var startNr = +raceNr[obj.length - 1] + 1;
+    for (var i = obj.length + 1; i <= 11; i++) {
+      dataPack1.push(0);
+      dataPack2.push(0);
+      dataPack3.push(0);
+      raceNr.push(startNr);
+      startNr++;
+    }
+  }
 
 
- chart.updateOptions({
- xaxis: {
-   categories: raceNr
- },
- })
- chart.updateSeries([{
-     name: 'Stora',
-     data: dataPack1
- },{
-     name: 'Små',
-     data: dataPack2
- },{
-     name: 'Dubbla',
-     data: dataPack3
- }]);
+  chart.updateOptions({
+    xaxis: {
+      categories: raceNr
+    },
+  })
+  chart.updateSeries([{
+    name: 'Stora',
+    data: dataPack1
+  }, {
+    name: 'Små',
+    data: dataPack2
+  }, {
+    name: 'Dubbla',
+    data: dataPack3
+  }]);
 }
 
 function setActiveRaceAnnotations(activeRace) {
-  console.log("ball");
-  console.log(activeRace);
-  console.log(Number.isInteger(raceNr[5]));
+  console.log("AktivtRace: " + activeRace);
 
-  if (+activeRace>0) {
-    chart.clearAnnotations();
-    chart.addXaxisAnnotation({
-      x: +activeRace,
-      borderColor: "blue",
-      label: {
-        borderColor: "black",
-        style: {
-          color: "#fff",
-          background: "blue"
+
+  if (+activeRace > 0) {
+    if (+(document.getElementById("outputEditLopp").innerHTML) >= 6 &&
+      +activeRace < (+(document.getElementById("outputEditLopp").innerHTML) + 6) &&
+      +activeRace > (+(document.getElementById("outputEditLopp").innerHTML) - 6)
+    ) {
+      chart.clearAnnotations();
+      chart.addXaxisAnnotation({
+        x: +activeRace,
+        borderColor: "blue",
+        label: {
+          borderColor: "black",
+          style: {
+            color: "#fff",
+            background: "blue"
+          },
+          text: 'Aktivt race'
         },
-        text: 'Aktivt race'
-      },
-    });
+      });
+
+    }
 
     chart.addYaxisAnnotation({
-      position: 'front' ,
+      position: 'front',
       y: 10,
       borderColor: "red",
       label: {
